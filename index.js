63a1ca6b09180b35fa32fcd672c4036f46c985f2
Miriam-Birungi <birungimiriam323@gmail.com>
2024-07-13 20:22:00 +0300

    clock commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const hourEl = document.querySelector("..hour")
+const minuteEl = document.querySelector(".minute")
+const secondEl = document.querySelector(".second")
+
+function updateClock() {
+    const currentDate = new Date();
+    console.log(currentDate);
+}
+
+updateClock();
